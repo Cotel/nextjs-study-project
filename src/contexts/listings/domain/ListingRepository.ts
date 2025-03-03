3de56418c28e26d636eba049686f9f369a020312
Cotel <miguelcoletomunoz@hotmail.com>
2025-03-03 14:30:58 +0100

    chore: cleaning project to start from 0

--- src/contexts/listings/domain/ListingRepository.ts
+++
@@ -1,9 +0,0 @@
-import { Listing } from './Listing'
-
-export interface ListingRepository {
-  findAll(): Promise<Listing[]>
-
-  findOne(id: string): Promise<Listing>
-
-  save(listing: Listing): Promise<Listing>
-}
